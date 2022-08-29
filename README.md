bc390721154e6b7e9dacd545a2151986f0858eb4
Gorr <gorrgorrob@gmail.com>
2022-08-29 20:56:03 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BuildTheGuild
+BuildTheGuild game
---
+++ src/main/java/net/catstack/buildtheguild/BuildTheGuildApplication.java
@@ -0,0 +1,13 @@
+package net.catstack.buildtheguild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildTheGuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildTheGuildApplication.class, args);
+    }
+
+}
