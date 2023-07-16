cb21c878872acba98f545314b9f6f588f8adb5a6
Gorr <gorrgorrob@gmail.com>
2023-07-16 13:00:27 +0500

    Fix tests

--- src/test/java/net/catstack/buildtheguild/BuildTheGuildApplicationTests.java
+++ src/test/java/net/catstack/buildtheguild/BuildTheGuildApplicationTests.java
@@ -3,7 +3,6 @@ package net.catstack.buildtheguild;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BuildTheGuildApplicationTests {
 
     @Test
